f39c14f6dd26ba1c559f4df78b09082625f8261e
DESKTOP-GANK31I\luantm <ngoalongtb001@gmail.com>
2018-02-21 19:40:24 +0700

    tet

--- QuanLyDiem/QuanLyDiem/Controllers/LoginController.cs
+++ QuanLyDiem/QuanLyDiem/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,30 @@ namespace QuanLyDiem.Controllers
 {
     public class LoginController : Controller
     {
+        public HighSchool db = new HighSchool();
         // GET: Login
         public ActionResult Index()
         {
+            ViewBag.Message = TempData["Message"];
             return View();
+        }
+        //POST 
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var temp = db.TaiKhoans.Where(x => x.tai_khoan == username && x.mat_khau == password);
+            if(temp.Count() == 1)
+            {
+                TaiKhoan loginAccount = temp.SingleOrDefault();
+                loginAccount.mat_khau = "";
+                Session.Add("LoginAccount", loginAccount);
+                return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+            }else
+            {
+                TempData["Message"] = "Tài khoản hoặc mật khẩu không chính xác";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
